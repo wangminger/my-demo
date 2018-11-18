54146f3fa2a6ab6e04289951cb31cb2557550ebc
wangminger <630195746@qq.com>
2018-11-18 11:35:49 +0800

    delete

--- project/test2.js
+++ project/test2.js
@@ -1,4 +1,4 @@
-var a=1,b=1;
+/*var a=1,b=1;
 for (var i = 0; i < 5; i++) {
 	for (var j=0; j<4; j++) {
 		if(a<10) {
@@ -9,4 +9,39 @@ for (var i = 0; i < 5; i++) {
 		}
 	}
 }
-console.log(a,b)
+console.log(a,b)*/
+
+//造塔，星星塔
+/*function towerBuilder(nFloors) {
+	// build here
+	var tower = [];
+	var star = "";
+	var length = nFloors;
+	for (let i = 1; i <= nFloors; i++) {
+		for (let m=0; m<length-i; m++) {
+			star += "-";
+		}
+		for (let n=0; n<2*i-1;n++) {
+			star += "*";
+		}
+		for (let l=0; l<length-i;l++) {
+			star+= "-";
+		}
+		tower.push(star);
+		star = ""
+	}
+	return tower;
+}
+console.log(towerBuilder(3))*/
+
+
+
+function simplify(poly){
+  //your code here
+  for (var i = 0; i < 2; i++) {
+  	console.log(i);
+  }
+	console.log(i);
+}
+
+console.log([1][0])
--- 表单/js/event.js
+++
@@ -1,9 +0,0 @@
-var EventUtil = {
-	addHander : function(element, type, handler) {
-		if (element.addEventListener) {
-			element.addEventListener(type, handler, false);
-		}else if (element.attachEvent) {
-			element.attachEvent(    )
-		}
-	}
-}
